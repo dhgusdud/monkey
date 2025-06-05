ee5e617741aedffdc55de91721fa4d21d020b564
dhgusdud <kate73226614@gmail.com>
2025-06-05 15:44:54 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2swdDhuODBtcXE1OW53b3dqdWJ0cTBkcG9wc2E3dHA4cmdwand3MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5VKbvrjxpVJCM/giphy.gif")
