bc00f250d26a82a6d99924b60aef67ce2a600692
dhgusdud <kate73226614@gmail.com>
2025-06-05 15:49:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ if emotion:
 
 st.markdown("---")
 st.markdown("<center>Made with ❤️ using Streamlit</center>", unsafe_allow_html=True)
-import streamlit as st
+
 st.image("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2swdDhuODBtcXE1OW53b3dqdWJ0cTBkcG9wc2E3dHA4cmdwand3MSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5VKbvrjxpVJCM/giphy.gif")
